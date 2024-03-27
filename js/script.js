3ac2e74dedded39b5974f8ff9fbfe1efa527207e
faithyun <156924177+faithyun@users.noreply.github.com>
2024-03-27 09:51:36 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+
+// Answers
+
+// Question 1
+
+document.querySelector("#host-name").innerHTML = "Faith"
+document.querySelector("#collaborator-name").innerHTML = "Peter"
+
+
+
+
+// Question 2
+// You can also create their own CSS classes in style.css file and use that. 
+
+// document.querySelector("#instruction-button").addEventListener("click", document.querySelectorAll(".question"))
+
+
+
+// Question 3
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+// Question 4
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+// Question 5
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+
+//Question 6
+
+
+
+
+
+//Question 7 
+
+
+
+
+
+//Question 8
+
+let colorPicker = document.querySelector('#color-picker');
+// colorPicker.addEventListener("change", document.querySelector("body").style.background.color = event.target.value)
+colorPicker.addEventListener("change", colorChange)
+
+function colorChange(event){
+    console.log(event.target.value);
+    document.querySelector("body").style.backgroundColor= event.target.value;
+}
+
+
+
+
+//Question 9 
+
